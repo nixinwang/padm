1443ba18f91487dfa16e2d5cc5e46a310010b5ac
nixin <nixin_wang@keysight.com>
2017-04-05 11:46:32 -0600

    enhanced to interrupt infinite task.

--- run_task_infinite.py
+++ run_task_infinite.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 import time
+import signal
+import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cmd')
@@ -12,8 +14,16 @@ pare_dir = os.path.dirname(curr_dir)
 
 cmd = f"python {os.path.join(curr_dir, 'run_task_multi_core.py')} --cmd '{args.cmd}' --num {args.num}"
 
+def signal_handler(signal, frame):
+    print('You pressed CTRL+C')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 while True:
     start = time.time()
     os.system(cmd)
     stop = time.time()
     print(f"Used Time: {stop-start}")
+
+
